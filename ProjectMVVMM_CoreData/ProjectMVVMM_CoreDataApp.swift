4d80c86e19dc4a46a81255135bfc0c4f34f6c49a
Hanitha <hanitha.raghavad@gmail.com>
2026-06-12 09:51:06 -0500

    Initial Commit

---
+++ ProjectMVVMM_CoreData/ProjectMVVMM_CoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectMVVMM_CoreDataApp.swift
+//  ProjectMVVMM_CoreData
+//
+//  Created by Hanitha Dhavileswarapu on 6/12/26.
+//
+
+import SwiftUI
+
+@main
+struct ProjectMVVMM_CoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
